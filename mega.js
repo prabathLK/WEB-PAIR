97d96b05798c07455d5b3de5f27a55a10e7df57e
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2024-08-15 17:44:09 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'EnterYourMegaAccountEmail',
-    password: 'EnterYourMegaAccountPassword',
+    email: 'doveha3110@iteradev.com',
+    password: 'Prabath@1234',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
